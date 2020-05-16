fcfd0a8087d93ac1a03b9d693b5afabcd7c5f02f
hoibuikms <hoibui@kms-technology.com>
2020-05-16 23:56:54 +0700

    Repository pattern

---
+++ Contracts/IRepositoryWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts
+{
+	public interface IRepositoryWrapper
+	{
+		IAccountRepository Account { get; }
+		IOwnerRepository Owner { get; }
+
+		void Save();
+	}
+}
---
+++ Repository/RepositoryWrapper/RepositoryWrapper.cs
@@ -0,0 +1,51 @@
+﻿using Contracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.RepositoryWrapper
+{
+	public class RepositoryWrapper : IRepositoryWrapper
+	{
+		private IOwnerRepository ownerRepository;
+		private IAccountRepository accountRepository;
+		private RepositoryContext repositoryContext;
+
+		public RepositoryWrapper(RepositoryContext repositoryContext)
+		{
+			this.repositoryContext = repositoryContext;
+		}
+
+		public IAccountRepository Account
+		{
+			get
+			{
+				if (accountRepository == null)
+				{
+					accountRepository = new AccountRepository(repositoryContext);
+				}
+
+				return accountRepository;
+			}
+		}
+
+		public IOwnerRepository Owner
+		{
+			get
+			{
+
+				if (ownerRepository == null)
+				{
+					ownerRepository = new OwnerRepository(repositoryContext);
+				}
+				return ownerRepository;
+			}
+		}
+
+		public void Save()
+		{
+			repositoryContext.SaveChanges();
+		}
+	}
+}
